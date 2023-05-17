fc9b615d2a1f0f6cf7da2dbefcbf3741bb86cf14
taranek <t.taranek27@gmail.com>
2023-05-17 20:55:52 +0200

    feature(): support env files

--- src/gql/apolloClient.ts
+++ src/gql/apolloClient.ts
@@ -2,10 +2,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { getSdk } from "@taranek/gql-client";
 import { GraphQLClient } from "graphql-request";
 
+const serverUrl = import.meta.env.VITE_PROXY_SERVER_URL || '';
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: `http://localhost:8080/graphql/v1`,
+  uri: `${serverUrl}/graphql/v1`,
 });
 export const sdkClient = getSdk(
-  new GraphQLClient(`http://localhost:8080/graphql/v1`, {})
+  new GraphQLClient(`${serverUrl}/graphql/v1`, {})
 );
