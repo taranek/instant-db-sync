9c00b48f5bed052118c951d832ac25854af6f0db
taranek <t.taranek27@gmail.com>
2023-05-23 22:47:02 +0200

    fix(client): fix render deploy

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,11 @@ import dotenv from "dotenv";
 import cors from "cors";
 import proxy from "express-http-proxy";
 
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
+
+// Create a single supabase client for interacting with your database
+
 dotenv.config();
 
 const app: Express = express();
@@ -10,6 +15,23 @@ const port = process.env.PORT || 8080;
 
 const supabaseServerUrl = process.env.SUPABASE_URL || "";
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
+const supabase = createClient<Database>(supabaseServerUrl, supabaseAnonKey);
+
+supabase.channel("subscription").on(
+  "postgres_changes",
+  {
+    event: "*",
+    schema: "public",
+    table: "Issues",
+  },
+  (payload) => {
+    console.log("Changes!", payload);
+  }
+);
+console.log(
+  "Supabase running on topics",
+  supabase.getChannels().map((c) => c.topic)
+);
 app.use(cors());
 app.use(
   "/",
